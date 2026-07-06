f8917a627843802663a0dae42f8bcc0abc594f3e
ditim15 <ditim456@gmail.com>
2026-07-06 15:37:34 -0400

    Added server start logic

--- server.js
+++ server.js
@@ -8,3 +8,22 @@ dotenv.config({
 const app = express();
 
 app.use(express.json());
+
+const startServer = async () => {
+    try {
+        app.on("error", (err) => {
+            console.log("Error", err);
+            throw err;
+        });
+
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`Server is running on port ${process.env.PORT}`);
+        });
+    } catch (err) {
+        console.log("Error starting server: ", err);
+    }
+} 
+
+startServer();
+
+export default app;
