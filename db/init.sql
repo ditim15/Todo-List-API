cea1f4667322bbd7d6dd10da5682e33edbe5dacf
ditim15 <ditim456@gmail.com>
2026-07-06 16:13:51 -0400

    Added Table creation SQL

---
+++ db/init.sql
@@ -0,0 +1,16 @@
+CREATE TABLE IF NOT EXISTS "users" (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    password VARCHAR(100) NOT NULL,
+);
+
+CREATE TABLE IF NOT EXISTS "todos" (
+    id SERIAL PRIMARY KEY,
+    user_id INT REFERENCES users(id) ON DELETE CASCADE,
+    title VARCHAR(255) NOT NULL,
+    description TEXT,
+    completed BOOLEAN NOT NULL DEFAULT false,
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMP NOT NULL DEFAULT NOW()
+);
