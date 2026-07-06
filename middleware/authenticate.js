6e912fc6bb01cd57ee6eaf5248d73ab0e1bfedd1
ditim15 <ditim456@gmail.com>
2026-07-06 16:07:38 -0400

    Added authenticate.js skeleton

---
+++ middleware/authenticate.js
@@ -0,0 +1,13 @@
+const authenticate = (req, res, next) => {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+        return res.status(401).json({ message: "No token provided" });
+    }
+
+    next();
+}
+
+export {
+    authenticate
+}
