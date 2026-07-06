867b2266e631319be688d9b72f420cddc8abe5f9
ditim15 <ditim456@gmail.com>
2026-07-06 15:52:05 -0400

    added login skeleton to auth controller

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -2,8 +2,22 @@ const registerUser = async (req, res, next) => {
     try {
         const {username, email, password } = req.bodyl
 
-        res.status(201).json({ message: "User registered successfully" });
+        res.status(201).json({ 
+            message: "User registered successfully" 
+        });
     } catch (err) {
         next(err);
     }
-}
+};
+
+const loginUser = async (req, res, next) => {
+    try {
+        const {email, password } = req.body;
+
+        res.status(200).json({ 
+            message: "User logged in successfully" 
+        });
+    } catch (err) {
+        next(err);
+    }
+};
